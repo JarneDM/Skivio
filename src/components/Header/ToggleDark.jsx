4387a0f5b859986618c005e61407940498593d39
JarneDM <jarne.demeyer05@hotmail.com>
2025-12-08 13:18:23 +0100

    changing icon darkmode and lightmode

--- src/components/Header/ToggleDark.jsx
+++ src/components/Header/ToggleDark.jsx
@@ -1,20 +1,23 @@
-// import React, { useEffect, useState } from "react";
-import toggle from "../../assets/toggle.png";
+import { useState } from "react";
+// import toggle from "../../assets/toggle.png";
+import { Sun, MoonStar } from "lucide-react";
 
 function ToggleDark() {
   // const [dark, setDark] = useState(() => localStorage.getItem("theme") === "dark");
+  const [isDark, setIsDark] = useState(() => document.documentElement.classList.contains("dark"));
 
   const toggleDark = () => {
     document.documentElement.classList.toggle("dark");
+    setIsDark(!isDark);
   };
 
   return (
     <div>
       <button
         onClick={toggleDark}
-        className=" border-none bg-transparent p-0 m-0 flex items-center dark:transform dark:rotate-180 dark:transition-all transition-all"
+        className=" border-none bg-transparent p-0 m-0 flex items-center dark:transform dark:rotate-360 dark:transition-all transition-all"
       >
-        <img className="h-[2rem] invert" src={toggle} alt="Toggle Dark Mode" />
+        {!isDark ? <Sun className="h-6 w-6 text-yellow-400" /> : <MoonStar className="h-6 w-6" />}
       </button>
     </div>
   );
